fad44d8b7df097f4eb35053986c55d03148359c6
Bakyt <bakosyy@gmail.com>
2023-04-05 06:42:02 +0200

    Word 2 Pdf finished

--- src/convert/convert.js
+++ src/convert/convert.js
@@ -2,24 +2,136 @@ import "../css/style.css"
 import "../js/collapsible"
 import axios from "axios"
 
-let btn = document.querySelector("button")
+const tbody = document.querySelector("tbody")
+const fileName = tbody.querySelector(".convert-name")
+const convertingSpinner = tbody.querySelector(".converting-indicator")
+const downloadButton = tbody.querySelector(".download-button")
+const conversionHeading = document.querySelector(".converting-text")
+const conversionIntro = document.getElementById("converting-intro")
+const successMessage = conversionIntro.querySelector(".success-message")
+const processingMessage = conversionIntro.querySelector(".processing-message")
+const conversions = document.querySelector("table")
+
 let fileToken = new URLSearchParams(document.location.search).get("fileToken")
-let url = `http://converter.local/api/test-convert/${fileToken}`
 
-const btnClickHandler = () => {}
-axios
-  .get(url, {
-    responseType: "blob"
-  })
-  .then((res) => {
-    console.log(res)
+const axe = axios.create({
+  baseURL: "http://converter.local/api"
+})
 
+const updateFileInfo = (fileInfo) => {
+  tbody.classList.remove("hidden")
+
+  let name = fileInfo.file_name
+  name = name.replace(`${fileInfo.from_extension}`, `${fileInfo.to_extension}`)
+  fileName.innerText = name
+}
+
+const showDownloadLinks = (convertedFile) => {
+  fileName.classList.add(
+    "text-[#2487EB]",
+    "hover:text-[#1061b1]",
+    "hover:underline"
+  )
+
+  downloadButton.classList.replace("hidden", "inline-block")
+
+  const onDownloadHandler = () => {
     const link = document.createElement("a")
-    link.href = URL.createObjectURL(res.data)
-    link.innerText = "Download"
-    link.setAttribute("download", "file.pdf")
-    btn.after(link)
-  })
-  .catch((err) => console.log(err))
+    link.href = URL.createObjectURL(convertedFile)
+    link.setAttribute("download", fileName.innerText)
+    link.click()
+    URL.revokeObjectURL(link.href)
+  }
 
-btn.addEventListener("click", btnClickHandler)
+  fileName.addEventListener("click", onDownloadHandler)
+  downloadButton.addEventListener("click", onDownloadHandler)
+}
+
+const showFileSuccessInfo = () => {
+  conversionHeading.innerText = "All Done!"
+  conversionIntro.classList.add("border-[#c3e6cb]")
+  conversionIntro.classList.replace("bg-white", "bg-[#d4edda]")
+  successMessage.classList.remove("hidden")
+  processingMessage.classList.add("hidden")
+}
+
+const showFileNotFoundInfo = () => {
+  conversionHeading.innerText = "No files found"
+  processingMessage.classList.add("text-[#3B3B3B]")
+  processingMessage.innerHTML = `
+    <p>Sorry, we couldn't find details for any uploaded files.</p>
+    <p>If you entered an email address when converting we'll be in touch soon. If you converted files 24 hours ago they have now expired.</p>
+    <p>If you need further help please contact <a href="mailto:support@bakyt.com" class="text-[#2487EB] hover:text-[#1061b1] hover:underline">our support team</a>.</p>
+  `
+  conversions.classList.add("hidden")
+}
+
+const showConvertSpinner = () => {
+  convertingSpinner.classList.replace("hidden", "inline-flex")
+}
+
+const hideConvertSpinner = () => {
+  convertingSpinner.classList.replace("inline-flex", "hidden")
+}
+
+/* Request: Verifying if token is correct */
+const verifyToken = async () => {
+  try {
+    const req = await axe.get(`/verify/${fileToken}`)
+    return req.data
+  } catch (err) {}
+}
+
+/* Request: Checking if conversion is already done */
+const checkAlreadyConverted = async () => {
+  try {
+    const req = await axe.get(`/download/${fileToken}`, {
+      responseType: "blob"
+    })
+
+    return req.data
+  } catch (err) {}
+}
+
+/* Request: Convert file request */
+const convertFile = async () => {
+  try {
+    const req = await axe.get(`/convert/${fileToken}`, {
+      responseType: "blob"
+    })
+
+    return req.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+/* Implementation */
+const makeRequests = async () => {
+  const fileInfo = await verifyToken()
+
+  if (fileInfo) {
+    updateFileInfo(fileInfo)
+
+    const convertedFile = await checkAlreadyConverted()
+    console.log(convertedFile)
+    if (convertedFile) {
+      showDownloadLinks(convertedFile)
+      showFileSuccessInfo()
+    } else {
+      // If File is not converted yet
+      showConvertSpinner()
+
+      const convertedFile = await convertFile()
+      if (convertedFile) {
+        showDownloadLinks(convertedFile)
+        hideConvertSpinner()
+        showFileSuccessInfo()
+      }
+    }
+  } else {
+    showFileNotFoundInfo()
+  }
+}
+
+makeRequests()
